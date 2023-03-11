f66988a3cdde18173ec654f0438544a14fc3056a
Dilraj Dhillon <dilrajdhillon@gmail.com>
2023-03-10 19:01:33 -0800

    added CORS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 
 class Input(BaseModel):
